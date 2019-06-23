06422687b3933548891ac9a482920d1e30c1b3f9
ozzi7 <nielregen@gmail.com>
2019-06-23 16:58:48 +0200

    update

--- benchmark.py
+++ benchmark.py
@@ -1,10 +1,19 @@
 import numpy as np
 import timeit
+import random
+
 
 nof_loops = 100
 input_size = 1024
-input = np.float32(np.random.rand(input_size))
-output = np.float32(np.zeros(input_size))
+input_np = np.float32(np.random.rand(input_size))
+input = list()
+output_np = np.float32(np.zeros(input_size))
+output = list()
+
+for x in range (0, input_size):
+    input.append(random.random())
+for x in range(0, input_size):
+    output.append(0.0)
 
 
 def decorator(func):
@@ -17,12 +26,17 @@ def decorator(func):
 
 def mult_numpy():
     for i in range(nof_loops):
-        np.multiply(input,0.5)
+        np.multiply(input_np, 0.5)
+
+def add_numpy():
+    for i in range(nof_loops):
+        np.add(input_np, 0.5)
 
 def mult():
     for i in range(nof_loops):
         for j in range(input_size):
-            output[j] = input[j]*0.5
+            input[j]*0.5
 
 decorator(mult)()
 decorator(mult_numpy)()
+decorator(add_numpy)()
